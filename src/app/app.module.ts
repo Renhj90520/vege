0d270dca7417c62b4fe6c49d523a4f071c9aeb62
renhaojie <renhaojie520@live.cn>
2017-05-11 17:23:53 +0800

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { routing } from './routing';
+import { CartComponent } from './cart/cart.component';
+import { OrderlistComponent } from './orderlist/orderlist.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    CartComponent,
+    OrderlistComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
